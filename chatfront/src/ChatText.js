cedf26a99b2815d2753f424ff59f81862178820c
sokkanen <joel.sokkanen@helsinki.fi>
2019-05-12 13:24:34 +0300

    dynaamisesti säätyvä chat-ikkuna

--- chatfront/src/ChatText.js
+++ chatfront/src/ChatText.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
-const style = {
-    margin: '20px',
-    border: '5px solid black',
-    height: '150px',
-}
-
 const ChatText = ({messages, msgcount, visible}) => {
     if (!visible){
         return <div></div>
     }
+
+    const heightCounter = () => {
+        return msgcount * 25;
+    }
+
+    const style = {
+        margin: '20px',
+        border: '5px solid black',
+        height: `${heightCounter()}px`,
+        'background-color': 'gray',
+        color: 'white'
+    }
+
     return (
         <div style={style}>
             <ul>
